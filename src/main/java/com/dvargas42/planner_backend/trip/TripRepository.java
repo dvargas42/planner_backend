77f44d0823456027aa3ed92bb178386429626cce
dvargas42 <dvargas.eng@gmail.com>
2024-07-09 22:30:23 -0300

    feat: :tada: initial commit

---
+++ src/main/java/com/dvargas42/planner_backend/trip/TripRepository.java
@@ -0,0 +1,8 @@
+package com.dvargas42.planner_backend.trip;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TripRepository extends JpaRepository<Trip, UUID> {
+}
