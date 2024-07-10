12dee979c8a4e2b0401b8dcc3799f0b28d7d6a3d
dvargas42 <dvargas.eng@gmail.com>
2024-07-09 22:29:55 -0300

    feat: :tada: initial commit

---
+++ src/main/java/com/dvargas42/planner_backend/PlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dvargas42.planner_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerBackendApplication.class, args);
+	}
+
+}
