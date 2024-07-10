e9a9f109a142ccfad7ae8cfb0daa1fbe77c527d3
dvargas42 <dvargas.eng@gmail.com>
2024-07-10 15:19:42 -0300

    feat: :truck: move to folder module

---
+++ src/main/java/com/dvargas42/planner_backend/module/trip/TripCreateResponse.java
@@ -0,0 +1,7 @@
+package com.dvargas42.planner_backend.module.trip;
+
+import java.util.UUID;
+
+public record TripCreateResponse(
+        UUID tripId
+) { }
--- src/main/java/com/dvargas42/planner_backend/trip/TripCreateResponse.java
+++
@@ -1,6 +0,0 @@
-package com.dvargas42.planner_backend.trip;
-
-import java.util.UUID;
-
-public record TripCreateResponse(UUID tripId) {
-}
