9ad1fa6c33572fb1f71bd9c161b08c36e21c2569
dvargas42 <dvargas.eng@gmail.com>
2024-07-11 18:22:53 -0300

    feat: :tada: initial commit

---
+++ src/main/java/com/dvargas42/planner_backend/module/activity/Activity.java
@@ -0,0 +1,39 @@
+package com.dvargas42.planner_backend.module.activity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+import com.dvargas42.planner_backend.module.trip.Trip;
+
+@Entity(name = "activities")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(name = "occurs_at", nullable = false)
+    private LocalDateTime occursAt;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+
+    public Activity(String title, String occursAt, Trip trip) {
+        this.title = title;
+        this.occursAt = LocalDateTime.parse(occursAt, DateTimeFormatter.ISO_DATE_TIME);
+        this.trip = trip;
+    }
+}
