9ded7f9c17178fbab797d18928c6c8681533b3a9
dvargas42 <dvargas.eng@gmail.com>
2024-07-13 17:41:01 -0300

    refactor: :recycle: refactor LinkController

--- src/main/java/com/dvargas42/planner_backend/module/link/LinkController.java
+++ src/main/java/com/dvargas42/planner_backend/module/link/LinkController.java
@@ -1,7 +1,6 @@
 package com.dvargas42.planner_backend.module.link;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,34 +12,54 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dvargas42.planner_backend.module.trip.Trip;
-import com.dvargas42.planner_backend.module.trip.TripService;
+import com.dvargas42.planner_backend.module.link.dto.LinkCreateReqDTO;
+import com.dvargas42.planner_backend.module.link.dto.LinkCreateRespDTO;
+import com.dvargas42.planner_backend.module.link.dto.LinkGetAllRespDTO;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("/links")
+@Tag(name = "Link", description = "Link information")
 public class LinkController {
 
     @Autowired
     private LinkService linkService;
 
-    @Autowired
-    private TripService tripService;
+    @PostMapping("/")
+    @Operation(summary = "Link register", description = "This functionality is responsible for create a link to an trip")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+            @Content(schema = @Schema(implementation = LinkCreateRespDTO.class))
+        }),
+        @ApiResponse(responseCode = "400", description = "Trip not found")
+    })
+    public ResponseEntity<LinkCreateRespDTO> createLink(@RequestBody @Valid LinkCreateReqDTO payload) {
+        
+        LinkCreateRespDTO link = this.linkService.createLinkToEvent(payload);
 
-    @PostMapping("/{id}/links")
-    public ResponseEntity<LinkResponse> createLink(@PathVariable UUID id, @RequestBody LinkRequestPayload payload) {
-            Trip trip = tripService.findTrip(id);
-
-            LinkResponse linkResponse = this.linkService.saveLink(payload, trip);
-
-            return ResponseEntity.ok(linkResponse);
+        return ResponseEntity.ok(link);
     }
 
-    @GetMapping("/{id}/links")
-    public ResponseEntity<List<LinkData>> getAllLinks(@PathVariable UUID id) {
-        List<LinkData> linkList = this.linkService.getAllLinksFromEvent(id);
+    @GetMapping("/")
+    @Operation(summary = "Link search all", description = "This functionality is responsible for get all links of a trip")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+            @Content(schema = @Schema(implementation = LinkGetAllRespDTO.class))
+        }),
+        @ApiResponse(responseCode = "400", description = "Trip not found")
+    })
+    public ResponseEntity<List<LinkGetAllRespDTO>> getAllLinks(@PathVariable @NotNull UUID tripId) {
+
+        List<LinkGetAllRespDTO> linkList = this.linkService.getAllLinksFromEvent(tripId);
 
         return ResponseEntity.ok(linkList);
     }
-
-    
 }
