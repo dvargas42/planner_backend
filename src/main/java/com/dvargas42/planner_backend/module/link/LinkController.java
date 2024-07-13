d9bbc994992859b654da4669442a44c09d459293
dvargas42 <dvargas.eng@gmail.com>
2024-07-13 20:33:00 -0300

    fix: :bug: change type  httpmethod

--- src/main/java/com/dvargas42/planner_backend/module/link/LinkController.java
+++ src/main/java/com/dvargas42/planner_backend/module/link/LinkController.java
@@ -6,10 +6,10 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dvargas42.planner_backend.module.link.dto.LinkCreateReqDTO;
@@ -56,7 +56,7 @@ public class LinkController {
         }),
         @ApiResponse(responseCode = "400", description = "Trip not found")
     })
-    public ResponseEntity<List<LinkGetAllRespDTO>> getAllLinks(@PathVariable @NotNull UUID tripId) {
+    public ResponseEntity<List<LinkGetAllRespDTO>> getAllLinks(@RequestParam @NotNull UUID tripId) {
 
         List<LinkGetAllRespDTO> linkList = this.linkService.getAllLinksFromEvent(tripId);
 
