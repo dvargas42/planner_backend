9b1f76ab388e6a62a77aadd049c047bb8ba6b25d
dvargas42 <dvargas.eng@gmail.com>
2024-07-13 17:28:59 -0300

    feat: :truck: change name

--- src/main/java/com/dvargas42/planner_backend/module/link/LinkRequestPayload.java
+++
@@ -1,6 +0,0 @@
-package com.dvargas42.planner_backend.module.link;
-
-public record LinkRequestPayload(
-    String title,
-    String url
-) {}
---
+++ src/main/java/com/dvargas42/planner_backend/module/link/dto/LinkCreateReqDTO.java
@@ -0,0 +1,26 @@
+package com.dvargas42.planner_backend.module.link.dto;
+
+import java.util.UUID;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record LinkCreateReqDTO(
+
+    @NotNull
+    UUID trip_id,
+
+    @NotBlank
+    @Pattern(regexp = "^$|.*\\s.*", message = "must not have one word")
+    @Pattern(regexp = "^([A-Za-zÀ-ÖØ-öø-ÿ0-9\\s]*)$", message = "must not have special characters")
+    @Length(max = 255, message = "must not have more than 255 characters")
+    String title,
+
+    @URL(message = "must have a valid url")
+    @Length(max = 255, message = "must not have more than 255 characters")
+    String url
+) {}
