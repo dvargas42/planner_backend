029c872a49fec74daccda7973288bbe22a748eb9
dvargas42 <dvargas.eng@gmail.com>
2024-07-13 17:32:29 -0300

    feat: :truck: change name

--- src/main/java/com/dvargas42/planner_backend/module/participant/ParticipantRequestPayload.java
+++
@@ -1,7 +0,0 @@
-package com.dvargas42.planner_backend.module.participant;
-
-public record ParticipantRequestPayload(
-        String name,
-        String email
-) {
-}
---
+++ src/main/java/com/dvargas42/planner_backend/module/participant/dto/ParticipantConfirmReqDTO.java
@@ -0,0 +1,20 @@
+package com.dvargas42.planner_backend.module.participant.dto;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record ParticipantConfirmReqDTO(
+
+    @NotBlank
+    @Pattern(regexp = "^$|.*\\s.*", message = "must not have one word")
+    @Pattern(regexp = "^([A-Za-zÀ-ÖØ-öø-ÿ0-9\\s]*)$", message = "must not have special characters")
+    @Length(max = 255, message = "must not have more than 255 characters")
+    String name,
+
+    @Email(message = "must have an valid e-mail")
+    @Length(max = 255, message = "must not have more than 255 characters")
+    String email
+) {}
