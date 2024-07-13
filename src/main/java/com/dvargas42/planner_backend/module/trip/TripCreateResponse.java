6b9782eb4e7b33866486a01fbac447f1d09b17cb
dvargas42 <dvargas.eng@gmail.com>
2024-07-13 17:45:22 -0300

    feat: :truck: change name

--- src/main/java/com/dvargas42/planner_backend/module/trip/TripCreateResponse.java
+++
@@ -1,7 +0,0 @@
-package com.dvargas42.planner_backend.module.trip;
-
-import java.util.UUID;
-
-public record TripCreateResponse(
-        UUID tripId
-) { }
---
+++ src/main/java/com/dvargas42/planner_backend/module/trip/dto/TripCreateRespDTO.java
@@ -0,0 +1,7 @@
+package com.dvargas42.planner_backend.module.trip.dto;
+
+import java.util.UUID;
+
+public record TripCreateRespDTO(
+        UUID tripId
+) { }
