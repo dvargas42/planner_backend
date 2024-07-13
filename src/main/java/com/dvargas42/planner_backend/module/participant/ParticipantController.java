8318c9b576a755dff7b14679837bfed1c54483c3
dvargas42 <dvargas.eng@gmail.com>
2024-07-13 17:34:04 -0300

    feat: :recycle: reafact ParticipantController

--- src/main/java/com/dvargas42/planner_backend/module/participant/ParticipantController.java
+++ src/main/java/com/dvargas42/planner_backend/module/participant/ParticipantController.java
@@ -1,31 +1,90 @@
 package com.dvargas42.planner_backend.module.participant;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-import java.util.UUID;
+import com.dvargas42.planner_backend.module.participant.dto.ParticipantConfirmReqDTO;
+import com.dvargas42.planner_backend.module.participant.dto.ParticipantConfirmRespDTO;
+import com.dvargas42.planner_backend.module.participant.dto.ParticipantGetAllRespDTO;
+import com.dvargas42.planner_backend.module.participant.dto.ParticipantInviteReqDTO;
+import com.dvargas42.planner_backend.module.participant.dto.ParticipantInviteRespDTO;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("/participants")
+@Tag(name = "Participant", description = "Participant information")
 public class ParticipantController {
 
     @Autowired
-    private ParticipantRepository participantRepository;
+    private ParticipantService participantService;
 
-    @PostMapping("/{id}/confirm")
-    public ResponseEntity<Participant> confirmParticipant(@PathVariable UUID id, @RequestBody ParticipantRequestPayload payload) {
-        Optional<Participant> participant = participantRepository.findById(id);
+    @GetMapping("/")
+    @Operation(summary = "Participant search all", description = "This functionality is responsible for get all participants of a trip")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+            @Content(schema = @Schema(implementation = ParticipantGetAllRespDTO.class))
+        }),
+        @ApiResponse(responseCode = "400", description = "Trip not found")
+    })
+    public ResponseEntity<List<ParticipantGetAllRespDTO>> getAllParticipantsFromEvent(
+            @RequestParam @NotNull UUID tripId) {
+                
+        List<ParticipantGetAllRespDTO> participantList = this.participantService
+                .getAllParticipantsFromEvent(tripId);
 
-        if (participant.isPresent()) {
-            Participant rawParticipant = participant.get();
-            rawParticipant.setIsConfirmed(true);
-            rawParticipant.setName(payload.name());
+        return ResponseEntity.ok(participantList);
+    }
 
-            this.participantRepository.save(rawParticipant);
-            return ResponseEntity.ok(rawParticipant);
-        }
-        return ResponseEntity.notFound().build();
+    @PatchMapping("/{participantId}")
+    @Operation(summary = "Participant register", description = "This functionality is responsible for confirm a participant")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+            @Content(schema = @Schema(implementation = ParticipantConfirmRespDTO.class))
+        }),
+        @ApiResponse(responseCode = "400", description = "Trip not found")
+    })
+    public ResponseEntity<ParticipantConfirmRespDTO> comfirmParticipantToEvent(
+            @PathVariable @NotNull UUID participantId,
+            @RequestBody @Valid ParticipantConfirmReqDTO payload) {
+    
+        ParticipantConfirmRespDTO participant = this.participantService
+                .comfirmParticipantToEvent(participantId, payload);
+
+        return ResponseEntity.ok(participant);
+    }
+
+    @PostMapping("/invites")
+    @Operation(summary = "Participant invite", description = "This functionality is responsible for invite a participant")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+            @Content(schema = @Schema(implementation = ParticipantInviteRespDTO.class))
+        }),
+        @ApiResponse(responseCode = "400", description = "Trip not found")
+    })
+    public ResponseEntity<ParticipantInviteRespDTO> inviteParticipantToEvent(
+            @RequestBody @Valid ParticipantInviteReqDTO payload) {
+
+        var participantId = this.participantService.inviteParticipanteToEvent(payload);
+
+        return ResponseEntity.ok(participantId);
     }
 }
