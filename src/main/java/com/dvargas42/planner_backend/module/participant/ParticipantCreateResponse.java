cce09fa72983775c13e0646d9df00b1704bd226a
dvargas42 <dvargas.eng@gmail.com>
2024-07-13 17:42:04 -0300

    feat: :truck: change name

--- src/main/java/com/dvargas42/planner_backend/module/participant/ParticipantCreateResponse.java
+++
@@ -1,6 +0,0 @@
-package com.dvargas42.planner_backend.module.participant;
-
-import java.util.UUID;
-
-public record ParticipantCreateResponse(UUID id) {
-}
---
+++ src/main/java/com/dvargas42/planner_backend/module/participant/dto/ParticipantInviteRespDTO.java
@@ -0,0 +1,7 @@
+package com.dvargas42.planner_backend.module.participant.dto;
+
+import java.util.UUID;
+
+public record ParticipantInviteRespDTO(
+    UUID id
+) {}
