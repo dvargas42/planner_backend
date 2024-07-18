0fe79b7087e45d8ea87d1147f5f65d55e8be7e93
dvargas42 <dvargas.eng@gmail.com>
2024-07-17 21:59:35 -0300

    test: :test_tube: create global variables in TripControllerUpdateTest

--- src/test/java/com/dvargas42/planner_backend/module/trip/TripControllerUpdateTest.java
+++ src/test/java/com/dvargas42/planner_backend/module/trip/TripControllerUpdateTest.java
@@ -7,8 +7,12 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -103,8 +107,25 @@ class TripControllerUpdateTest {
         assertEquals(OWNER_EMAIL, tripResponseUpdate.owner_email());
     }
 
-    @Test
-    void shouldToBeNotAbleToUpdatWhenStartsAtInPast() throws Exception {
+    private static Stream<Arguments> provideDateInterval() {
+        return Stream.of(
+            Arguments.of("localTest, LT",LocalDateTime.now().plusDays(1)   , LocalDateTime.now()),
+            Arguments.of("localTest, LT",LocalDateTime.now()                    , LocalDateTime.now().plusDays(1)),
+            Arguments.of("localTest, LT",LocalDateTime.now().minusDays(1)  , LocalDateTime.now().plusDays(1)),
+            Arguments.of("loc@lTest, LT",LocalDateTime.now().plusDays(2)   , LocalDateTime.now().plusDays(4)),
+            Arguments.of(" "            ,LocalDateTime.now().plusDays(2)   , LocalDateTime.now().plusDays(4)),
+            Arguments.of(""             ,LocalDateTime.now().plusDays(2)   , LocalDateTime.now().plusDays(4)),
+            Arguments.of(null           ,LocalDateTime.now().plusDays(2)   , LocalDateTime.now().plusDays(4))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideDateInterval")
+    void shouldToBeNotAbleToUpdatWhenStartsAtAndEndsAtAreInvalid(
+        String destinationUpdate,
+        LocalDateTime startsAtUpdate,
+        LocalDateTime endsAtUpdate
+    ) throws Exception {
         String DESTINATION = "LOCALTEST, TS";
         String OWNER_EMAIL = "owner@gmail.com.br";
         String OWNER_NAME = "OWNERFIRSTNAME OWNERLASTNAME";
@@ -133,16 +154,11 @@ class TripControllerUpdateTest {
         );
         TripCreateRespDTO tripResponseCreate = objectMapper
                 .readValue(responsePost.getBody(), TripCreateRespDTO.class);
-        
-
-        String DESTINATION_UPDATE = "LOCALTEST UPDATE, TS";
-        String STARTS_AT_UPDATE = TestUtils.convertDateTime(LocalDateTime.now().minusDays(2));
-        String ENDS_AT_UPDATE = TestUtils.convertDateTime(LocalDateTime.now().plusDays(4));
 
         TripUpdateReqDTO tripUpdateReqDTO = new TripUpdateReqDTO(
-            DESTINATION_UPDATE,
-            STARTS_AT_UPDATE,
-            ENDS_AT_UPDATE
+            destinationUpdate,
+            TestUtils.convertDateTime(startsAtUpdate),
+            TestUtils.convertDateTime(endsAtUpdate)
         );
 
         HttpHeaders headers = new HttpHeaders();
@@ -158,120 +174,5 @@ class TripControllerUpdateTest {
         );
 
         assertEquals(400, responsePatch.getStatusCode().value());
-    }
-
-    @Test
-    void shouldToBeNotAbleToUpdatWhenEndsAtLessThanStartsAt() throws Exception {
-        String DESTINATION = "LOCALTEST, TS";
-        String OWNER_EMAIL = "owner@gmail.com.br";
-        String OWNER_NAME = "OWNERFIRSTNAME OWNERLASTNAME";
-
-        List<String> EMAIL_LIST = new ArrayList<>();
-        EMAIL_LIST.add("participant01@gmail.com");
-        EMAIL_LIST.add("participant02@gmail.com");
-        EMAIL_LIST.add("participant03@gmail.com");
-
-        String STARTS_AT = TestUtils.convertDateTime(LocalDateTime.now().plusDays(1));
-        String ENDS_AT = TestUtils.convertDateTime(LocalDateTime.now().plusDays(2));
-
-        TripCreateReqDTO tripRequest = new TripCreateReqDTO(
-            DESTINATION,
-            STARTS_AT,
-            ENDS_AT,
-            EMAIL_LIST,
-            OWNER_EMAIL,
-            OWNER_NAME
-        );
-
-        ResponseEntity<String> responsePost = restTemplate.postForEntity(
-            "/trips/", 
-            tripRequest, 
-            String.class
-        );
-        TripCreateRespDTO tripResponseCreate = objectMapper
-                .readValue(responsePost.getBody(), TripCreateRespDTO.class);
-        
-
-        String DESTINATION_UPDATE = "LOCALTEST UPDATE, TS";
-        String STARTS_AT_UPDATE = TestUtils.convertDateTime(LocalDateTime.now().plusDays(2));
-        String ENDS_AT_UPDATE = TestUtils.convertDateTime(LocalDateTime.now().plusDays(1));
-
-        TripUpdateReqDTO tripUpdateReqDTO = new TripUpdateReqDTO(
-            DESTINATION_UPDATE,
-            STARTS_AT_UPDATE,
-            ENDS_AT_UPDATE
-        );
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        HttpEntity<TripUpdateReqDTO> requestEntity = new HttpEntity<>(tripUpdateReqDTO, headers);
-
-        ResponseEntity<String> responsePatch = restTemplate.exchange(
-            "/trips/" + tripResponseCreate.tripId().toString(),
-            HttpMethod.PATCH,
-            requestEntity,
-            String.class
-        );
-
-        assertEquals(400, responsePatch.getStatusCode().value());
-    }
-
-    @Test
-    void shouldToBeNotAbleToUpdatWhenStartsAtIsEqualEndsAt() throws Exception {
-        String DESTINATION = "LOCALTEST, TS";
-        String OWNER_EMAIL = "owner@gmail.com.br";
-        String OWNER_NAME = "OWNERFIRSTNAME OWNERLASTNAME";
-
-        List<String> EMAIL_LIST = new ArrayList<>();
-        EMAIL_LIST.add("participant01@gmail.com");
-        EMAIL_LIST.add("participant02@gmail.com");
-        EMAIL_LIST.add("participant03@gmail.com");
-
-        String STARTS_AT = TestUtils.convertDateTime(LocalDateTime.now().plusDays(1));
-        String ENDS_AT = TestUtils.convertDateTime(LocalDateTime.now().plusDays(2));
-
-        TripCreateReqDTO tripRequest = new TripCreateReqDTO(
-            DESTINATION,
-            STARTS_AT,
-            ENDS_AT,
-            EMAIL_LIST,
-            OWNER_EMAIL,
-            OWNER_NAME
-        );
-
-        ResponseEntity<String> responsePost = restTemplate.postForEntity(
-            "/trips/", 
-            tripRequest, 
-            String.class
-        );
-        TripCreateRespDTO tripResponseCreate = objectMapper
-                .readValue(responsePost.getBody(), TripCreateRespDTO.class);
-        
-
-        String DESTINATION_UPDATE = "LOCALTEST UPDATE, TS";
-        String STARTS_AT_UPDATE = TestUtils.convertDateTime(LocalDateTime.now().plusDays(2));
-        String ENDS_AT_UPDATE = STARTS_AT_UPDATE;
-
-        TripUpdateReqDTO tripUpdateReqDTO = new TripUpdateReqDTO(
-            DESTINATION_UPDATE,
-            STARTS_AT_UPDATE,
-            ENDS_AT_UPDATE
-        );
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        HttpEntity<TripUpdateReqDTO> requestEntity = new HttpEntity<>(tripUpdateReqDTO, headers);
-
-        ResponseEntity<String> responsePatch = restTemplate.exchange(
-            "/trips/" + tripResponseCreate.tripId().toString(),
-            HttpMethod.PATCH,
-            requestEntity,
-            String.class
-        );
-
-        assertEquals(400, responsePatch.getStatusCode().value());
-    }
-    
+    } 
 }
